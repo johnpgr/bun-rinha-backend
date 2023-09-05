ef893c7592a04eb6f1f68e88a3b9a108772e5ccf
greidinger-reis <jp.greidinger@hotmail.com>
2023-09-05 11:38:46 -0300

    removed generated

--- generated/context/index.ts
+++
@@ -1,4 +0,0 @@
-import { db } from "@/database";
-import type { Elysia } from "elysia";
-export const appContext = (app: Elysia) => app.decorate("db", db);
-export type AppContext = ReturnType<typeof appContext>;
